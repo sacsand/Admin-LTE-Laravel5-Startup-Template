a979a3fea36ee2ebc8973597c24e74c928170370
sacsand <c4c5nf3@gmail.com>
2016-10-01 22:41:13 +0530

    ast

---
+++ resources/views/sample.blade.php
@@ -0,0 +1,26 @@
+@include('header')
+@include('sidebar')
+
+<div class="content-wrapper">
+    <section class="content-header">
+      <section class="content-header">
+        <h1>
+            Product
+            <small>Manage your product prices</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="/product/"><i class="fa fa-home"></i> Home</a></li>
+
+        </ol>
+      </section>
+
+    <section class="content">
+    contecnt here
+
+    </section>
+
+    </section>
+    </div>
+
+@include('footer')
+    
